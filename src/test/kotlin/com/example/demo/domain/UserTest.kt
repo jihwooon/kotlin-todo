1379d8a203f745aa0b7f449c4a5ee110e5bf61ce
jihwooon <jihwooon@gmail.com>
2022-10-03 10:47:04 +0900

    UserTest 계층 구조 테스트 코드를 작성하라

--- src/test/kotlin/com/example/demo/domain/UserTest.kt
+++ src/test/kotlin/com/example/demo/domain/UserTest.kt
@@ -1,20 +1,37 @@
 package com.example.demo.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-// TODO : User Unit 테스트를 하라
-internal class UserTest() {
+@DisplayName("User 클래스")
+internal class UserTest {
 
-    private lateinit var user: User
+    val user = User(id = 1004L, name = "abcd", email = "abc@gmail.com", password = "1234")
 
-    @Test
-    fun `User Unit Test`() {
-        user = User(id = 1004L, name = "abcd", email = "abc@gmail.com", password = "1234")
+    @Nested
+    @DisplayName("값이 주어진다면")
+    inner class DescribeOfValue {
+        @Test
+        fun `id 값을 리턴한다`() {
+            assertThat(user.id).isEqualTo(1004L)
+        }
 
-        assertThat(user.id).isEqualTo(1004L)
-        assertThat(user.name).isEqualTo("abcd")
-        assertThat(user.email).isEqualTo("abc@gmail.com")
-        assertThat(user.password).isEqualTo("1234")
+        @Test
+        fun `name 값을 리턴한다`() {
+            assertThat(user.name).isEqualTo("abcd")
+        }
+
+        @Test
+        fun `email 값을 리턴한다`() {
+            assertThat(user.email).isEqualTo("abc@gmail.com")
+        }
+
+        @Test
+        fun `password 값을 리턴한다`() {
+            assertThat(user.password).isEqualTo("1234")
+        }
     }
 }
+
