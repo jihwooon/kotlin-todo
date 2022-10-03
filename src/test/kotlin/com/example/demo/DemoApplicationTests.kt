182be436ce469550e94abbb03ca115421ecb2677
jihwooon <jihwooon@gmail.com>
2022-10-03 23:48:54 +0900

    Gradle 수정하기

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,22 @@
 package com.example.demo
 
+import com.example.demo.domain.ProductRepository
+import com.example.demo.domain.UserRepository
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 
-//@SpringBootTest
-//class DemoApplicationTests {
-//
-//    @Test
-//    fun contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+class DemoApplicationTests {
+
+    @MockBean
+    private lateinit var productRepository: ProductRepository
+
+    @MockBean
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
